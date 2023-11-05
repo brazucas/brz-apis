de9b50e8bab7fb82cb1eeefd35acaa93302dabb7
pedropapa <pedro.papadopolis@gmail.com>
2023-11-05 14:44:22 +1100

    feat: simplifying implementation

--- apps/notifications/src/adapters/dynamo.ts
+++ apps/notifications/src/adapters/dynamo.ts
@@ -8,74 +8,53 @@ import {
 } from "@aws-sdk/client-dynamodb";
 import { addSeconds } from "date-fns";
 
-export const writeCode = (
+export const writeCode = async (
   id: string,
   code: string,
   tries: number,
   nextTry = addSeconds(new Date(), Number(process.env.WAITING_TIME) || 180)
-): Promise<PutItemOutput> =>
-  new Promise((resolve, reject) => {
-    const db = new DynamoDB({ apiVersion: "2012-08-10", region: "us-east-1" });
+): Promise<PutItemOutput> => {
+  const db = new DynamoDB({ apiVersion: "2012-08-10", region: "us-east-1" });
 
-    const params: PutItemCommandInput = {
-      TableName: "CONFIRMATION_CODE",
-      Item: {
-        id: { S: id },
-        code: { S: code },
-        nextTry: { S: nextTry.toISOString() },
-        tries: { N: tries.toString() },
-      },
-    };
+  const params: PutItemCommandInput = {
+    TableName: "CONFIRMATION_CODE",
+    Item: {
+      id: { S: id },
+      code: { S: code },
+      nextTry: { S: nextTry.toISOString() },
+      tries: { N: tries.toString() },
+    },
+  };
 
-    db.putItem(params, function (err, data) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
-  });
+  return db.putItem(params);
+};
 
-export const readCode = (id: string): Promise<GetItemOutput> =>
-  new Promise((resolve, reject) => {
-    const db = new DynamoDB({ apiVersion: "2012-08-10", region: "us-east-1" });
+export const readCode = (id: string): Promise<GetItemOutput> => {
+  const db = new DynamoDB({ apiVersion: "2012-08-10", region: "us-east-1" });
 
-    const params: GetItemCommandInput = {
-      TableName: "CONFIRMATION_CODE",
-      Key: {
-        id: { S: id },
-      },
-    };
+  const params: GetItemCommandInput = {
+    TableName: "CONFIRMATION_CODE",
+    Key: {
+      id: { S: id },
+    },
+  };
 
-    db.getItem(params, function (err, data) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
-  });
+  return db.getItem(params);
+};
 
-export const confirmWhitelistCodes = (id: string): Promise<PutItemOutput> =>
-  new Promise((resolve, reject) => {
-    const db = new DynamoDB({ apiVersion: "2012-08-10", region: "us-east-1" });
+export const confirmWhitelistCodes = (id: string): Promise<PutItemOutput> => {
+  const db = new DynamoDB({ apiVersion: "2012-08-10", region: "us-east-1" });
 
-    const params: UpdateItemCommandInput = {
-      TableName: "WHITELIST_CONFIRMATION",
-      Key: {
-        steamId: { S: id },
-      },
-      UpdateExpression: "set isConfirmed = :yes",
-      ExpressionAttributeValues: {
-        ":yes": { BOOL: true },
-      },
-    };
+  const params: UpdateItemCommandInput = {
+    TableName: "WHITELIST_CONFIRMATION",
+    Key: {
+      steamId: { S: id },
+    },
+    UpdateExpression: "set isConfirmed = :yes",
+    ExpressionAttributeValues: {
+      ":yes": { BOOL: true },
+    },
+  };
 
-    db.updateItem(params, function (err, data) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
-  });
+  return db.updateItem(params);
+};
---
+++ apps/notifications/test/unit/adapters/dynamo.test.ts
@@ -0,0 +1,7 @@
+describe("DynamoDB Adapter", () => {
+  describe("writeCode", () => {
+    it("should write a code to DynamoDB", () => {
+      expect(true).toBe(true);
+    });
+  });
+});
