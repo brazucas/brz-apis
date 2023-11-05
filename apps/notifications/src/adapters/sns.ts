80e2ad7ea389bf01b86eb9206d15b07bdf10ff1a
pedropapa <pedro.papadopolis@gmail.com>
2023-11-05 15:16:51 +1100

    feat: sns tests

--- apps/notifications/src/adapters/sns.ts
+++ apps/notifications/src/adapters/sns.ts
@@ -1,32 +1,19 @@
 import { PublishInput, SNS } from "@aws-sdk/client-sns";
 
-export const sendSMS = (phoneNumber: string, message: string) =>
-  new Promise((resolve, reject) => {
-    const sns = new SNS({ region: "us-east-1" });
+export const sendSMS = async (phoneNumber: string, message: string) => {
+  const sns = new SNS({ region: "us-east-1" });
 
-    sns.setSMSAttributes(
-      {
-        attributes: {
-          DefaultSMSType: "Transactional",
-        },
-      },
-      (error: any) => {
-        console.error(error);
-        if (error) reject(error);
-      }
-    );
-
-    const params: PublishInput = {
-      PhoneNumber: phoneNumber,
-      Message: message,
-      MessageStructure: "string",
-    };
-
-    sns.publish(params, (err, data) => {
-      if (err) {
-        return reject(err);
-      }
-
-      resolve(true);
-    });
+  await sns.setSMSAttributes({
+    attributes: {
+      DefaultSMSType: "Transactional",
+    },
   });
+
+  const params: PublishInput = {
+    PhoneNumber: phoneNumber,
+    Message: message,
+    MessageStructure: "string",
+  };
+
+  return await sns.publish(params);
+};
