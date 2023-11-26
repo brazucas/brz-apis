01b47db328f8551e7f8b17c8132ad0c1844877c4
pedropapa <pedro.papadopolis@gmail.com>
2023-11-26 18:04:32 +1100

    feat(notifications): deploy behind private vpc

---
+++ apps/notifications/.cicd/terraform/outputs.tf
@@ -0,0 +1,19 @@
+output "notifications_sg_id" {
+  value = aws_security_group.subnet_sg.id
+}
+
+output "brz_vpc_private_subnet_0_id" {
+  value = data.terraform_remote_state.brz_state.outputs.brz_vpc_private_subnet_0_id
+}
+
+output "brz_vpn_private_ip" {
+  value = data.terraform_remote_state.brz_state.outputs.brz_vpn_private_ip
+}
+
+output "brz_vpc_nat_gateway_public_ip" {
+  value = data.terraform_remote_state.brz_state.outputs.brz_vpc_nat_gateway_public_ip
+}
+
+output "brz_vpc_nat_gateway_private_ip" {
+  value = data.terraform_remote_state.brz_state.outputs.brz_vpc_nat_gateway_private_ip
+}
