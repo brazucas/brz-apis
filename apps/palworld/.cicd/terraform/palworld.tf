e3f84e8c18f18ca25f48a91214c9c540b4147e2e
pedropapa <pedro.papadopolis@gmail.com>
2024-02-01 21:38:21 +1100

    feat: add palworld server

---
+++ apps/palworld/.cicd/terraform/palworld.tf
@@ -0,0 +1,77 @@
+resource "aws_key_pair" "palworld_keypair" {
+  key_name = "palworld_instance_keypair"
+  public_key = file("palworld_keypair.pub")
+  tags = {
+    Name = "palworld"
+  }
+}
+
+resource "aws_security_group" "palworld_server" {
+    name = "palworld_instance"
+    description = "Security group for palworld server"
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 8211
+        to_port = 8211
+        protocol = "udp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_instance" "palworld_server" {
+    ami = "ami-0162b990068ef0498"
+    availability_zone = "sa-east-1c"
+    instance_type = "t2.2xlarge"
+    key_name = "palworld_keypair"
+
+    vpc_security_group_ids = [aws_security_group.palworld_server.id]
+
+    root_block_device {
+        volume_type           = "gp2"
+        volume_size           = 8
+        delete_on_termination = false
+    }
+
+    connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ubuntu"
+      private_key = aws_key_pair.palworld_keypair.key_name
+      timeout     = "4m"
+   }
+}
+
+resource "aws_eip" "palworld_server" {
+    instance = aws_instance.palworld_server.id
+    vpc = true
+}
+
+resource "aws_route53_record" "palworld" {
+    name = "pal.brz.gg"
+    zone_id = data.terraform_remote_state.brz_state.outputs.brz_gg_zone_id
+    type = "A"
+    records = [
+        aws_eip.palworld_server.public_ip,
+    ]
+    ttl = "3600"
+}
+
+output "palworld_ip_address" {
+  value = aws_eip.palworld_server.public_ip
+}
+
+output "palworld_instance_id" {
+  value = aws_instance.palworld_server.id
+}
