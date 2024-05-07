4336ddf5494a69d4de02908dc27dcba949762189
pedropapa <pedro.papadopolis@gmail.com>
2024-05-07 18:50:16 +1000

    feat(notifications): removing unused resources

--- apps/notifications/.cicd/terraform/notifications_sg.tf
+++ apps/notifications/.cicd/terraform/notifications_sg.tf
@@ -1,27 +1,26 @@
 resource "aws_security_group" "subnet_sg" {
   name        = "notifications_sg"
   description = "Security Group for notifications service"
-  vpc_id      = data.terraform_remote_state.brz_state.outputs.brz_vpc_id
 
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = [data.terraform_remote_state.brz_state.outputs.brz_vpc_private_subnet_0_cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = [data.terraform_remote_state.brz_state.outputs.brz_vpc_private_subnet_0_cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
-    cidr_blocks = [data.terraform_remote_state.brz_state.outputs.brz_vpc_private_subnet_0_cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
